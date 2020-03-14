c3ad5416c8b39d8b3ffe13e6266f26bb9bdb3077
Jae Yoo <pppjqqq@gmail.com>
2020-03-14 11:40:07 -0700

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -20,34 +20,24 @@ const styles = theme => ({
   }
 })
 
-const customers = [
-  {
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/1',
-  'name': '유재돈',
-  'birthday': '870502',
-  'gender': '남자',
-  'job': '소프트웨어 엔지니어'
-},
-{
-  'id': 2,
-  'image': 'https://placeimg.com/64/64/2',
-  'name': '홍길동',
-  'birthday': '810912',
-  'gender': '남자',
-  'job': '친구'
-},
-{
-  'id': 3,
-  'image': 'https://placeimg.com/64/64/3',
-  'name': '연예인',
-  'birthday': '930426',
-  'gender': '여자',
-  'job': '모델'
-}
-]
-
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const { classes } = this.props;
     return (
@@ -64,7 +54,9 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> ); }) }
+            {this.state.customers ? this.state.customers.map(c => { 
+              return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/> ); 
+            }) : ""}
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
-})
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            "id": 1,
+            "image": "https://placeimg.com/64/64/1",
+            "name": "유재돈",
+            "birthday": "870502",
+            "gender": "남자",
+            "job": "소프트웨어 엔지니어"
+        },
+        {
+            "id": 2,
+            "image": "https://placeimg.com/64/64/2",
+            "name": "홍길동",
+            "birthday": "810712",
+            "gender": "남자",
+            "job": "학생"
+        },
+        {
+            "id": 3,
+            "image": "https://placeimg.com/64/64/3",
+            "name": "연예인",
+            "birthday": "931205",
+            "gender": "여자",
+            "job": "모델"
+        }
+    ]);
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
